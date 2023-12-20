ca5152d4f32a888839f1fb74dc8687eb64e91b35
Sergarmor <108725112+Sergarmor@users.noreply.github.com>
2023-12-20 13:11:31 +0100

    Cálculo de la función de correlación
    Primer archivo

--- cor_function.py
+++ cor_function.py
@@ -1,20 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-# from corrfunc.theory import xi
+from corrfunc.theory import xi
 
-# data = np.load('sergio_ucm.npy')
+data = np.load('sergio_ucm.npy')
 
-# L = 205.0
+h=0.7
+L = 205.0
 
-# binfile = np.logspace(np.log10(0.1), np.log10(10.0), 15)
+X = data[:, 0]
+Y = data[:, 1]
+Z = data[:, 2]
 
-# results = xi(L, 1, binfile, X, Y, Z)
+binfile = np.logspace(np.log10(0.1), np.log10(10.0), 15)
 
-x=np.array([0, 1, 2, 3, 4, 5])
+results = xi(L, 1, binfile, X, Y, Z, verbose=True)
 
-y=np.array([1, 2, 3, 4, 5, 6])
-
-results = x+y
 
 np.save('results', results)
