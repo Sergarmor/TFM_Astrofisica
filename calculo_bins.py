cc7b09441046b0d02a5cfddec05f2802c77f1ca3
Sergarmor <108725112+Sergarmor@users.noreply.github.com>
2024-01-22 12:35:13 +0100

    Calculo bins en halos

--- calculo_bins.py
+++ calculo_bins.py
@@ -1,11 +1,25 @@
 import numpy as np
 import pandas as pd
 
+halos=pd.read_csv('Resultados/halos.csv')
 galaxies=pd.read_csv('Resultados/galaxies.csv')
 bin_width=0.1
 bins_masa=np.arange(10.7, 15.2, bin_width)
 
-m=galaxies['Halo mass']
+# m=galaxies['Halo mass']
+# bin_col=np.ones(len(m), dtype=int)
+# for j in range(len(m)):
+    # for i in range(len(bins_masa)):
+        # if m[j] > bins_masa[i] and m[j] < bins_masa[i+1]:
+            # bin_col[j]=int(i+1)
+            
+            # break
+        # else:
+            # continue
+# galaxies['Mass bin'] = bin_col
+# galaxies.to_csv('Resultados/galaxies.csv', index=False)
+
+m=halos['Halo mass']
 bin_col=np.ones(len(m), dtype=int)
 for j in range(len(m)):
     for i in range(len(bins_masa)):
@@ -15,5 +29,5 @@ for j in range(len(m)):
             break
         else:
             continue
-galaxies['Mass bin'] = bin_col
-galaxies.to_csv('Resultados/galaxies.csv', index=False)
+halos['Mass bin'] = bin_col
+halos.to_csv('Resultados/halos.csv', index=False)
