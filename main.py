14e7bbf0830f5255a77c3889f4c3fd8f2ac7ef15
Sergarmor <108725112+Sergarmor@users.noreply.github.com>
2024-01-22 16:46:44 +0100

    Added various seeds

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ h = 0.6774 # Little Hubble constant
 
 halos = pd.read_csv('Resultados/halos.csv')
 galaxies = pd.read_csv('Resultados/galaxies.csv')
-galaxies_shuffled = pd.read_csv('Resultados/galaxies_shuffled.csv')
+
 
 galaxies_sample = galaxies[galaxies['Stellar mass']>10.75]
 
@@ -19,16 +19,19 @@ X = galaxies_sample['Gal_x']
 Y = galaxies_sample['Gal_y']
 Z = galaxies_sample['Gal_z']
 
-X_shuffled = galaxies_shuffled['Gal_x_new']
-Y_shuffled = galaxies_shuffled['Gal_y_new']
-Z_shuffled = galaxies_shuffled['Gal_z_new']
-
 weights = np.ones_like(X)
-
 binfile = np.logspace(np.log10(0.1), bins_espaciales, 100)
 
 pcf_original = xi(L, 1, binfile, X, Y, Z, weights = weights)
-pcf_shuffled = xi(L, 1, binfile, X_shuffled, Y_shuffled, Z_shuffled, weights = weights)
+
+for q in range(10):
+    galaxies_shuffled = pd.read_csv(f'Resultados/galaxies_shuffled{q}.csv')
+
+    X_shuffled = galaxies_shuffled['Gal_x_new']
+    Y_shuffled = galaxies_shuffled['Gal_y_new']
+    Z_shuffled = galaxies_shuffled['Gal_z_new']
+
+    pcf_shuffled = xi(L, 1, binfile, X_shuffled, Y_shuffled, Z_shuffled, weights = weights)
 
 np.save('Resultados/pcf_original', pcf_original)
 np.save('Resultados/pcf_shuffled', pcf_shuffled)
