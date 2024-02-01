504607d59d181d3c653d83018523dd027bec882b
Sergarmor <108725112+Sergarmor@users.noreply.github.com>
2024-02-02 00:02:04 +0100

    Finished double binning

--- calculo_bins.py
+++ calculo_bins.py
@@ -1,41 +1,58 @@
-import numpy as np
-import pandas as pd
-import math
-
-halos=pd.read_csv('Resultados/halos.csv')
-galaxies=pd.read_csv('Resultados/galaxies.csv')
-
-# Parámetros a setear antes de ejecutar
-
-bin_feature = 'Halo mass'
-sub_bin_feature = 'Halo mass squared'
-
-bin_width=0.1
-sub_bin_width=0.1
 
 
-# Cálculo de los bins y binneado de las galaxias y los halos.
+def calculo_bins(bin_feature, sub_bin_feature, bin_width, sub_bin_width):
 
-limite_inferior_main_bin = math.floor(min(galaxies[bin_feature]) * 10.0) / 10.0
-limite_superior_main_bin = math.ceil(max(galaxies[bin_feature]) * 10.0) / 10.0+bin_width
+    """
+    Galaxy and halo DataFrame binning.
+    
+    Parameters
+    ----------
+    
+    bin_feature : string
+                  First feature to take account of for the binning
+    sub_bin_feature : string
+                  Second feature to take account of for the binning
+    bin_width : float
+                  Bin width of the first feature
+    sub_bin_width : float
+                  Bin width of the second feature
+                  
+    Returns
+    -------
+    
+    halos : DataFrame
+            DataFrame of binned halos
+    galaxies : DataFrame
+            DataFrame of binned galaxies
+    
+    """
 
-limite_inferior_sub_bin = math.floor(min(galaxies[sub_bin_feature]) * 10.0) / 10.0
-limite_superior_sub_bin = math.ceil(max(galaxies[sub_bin_feature]) * 10.0) / 10.0+sub_bin_width
+    import numpy as np
+    import pandas as pd
+    import math
 
-main_bins = np.arange(limite_inferior_main_bin, limite_superior_main_bin, bin_width)
-sub_bins = np.arange(limite_inferior_sub_bin, limite_superior_sub_bin, sub_bin_width)
+    limite_inferior_main_bin = math.floor(min(galaxies[bin_feature]) * 10.0) / 10.0
+    limite_superior_main_bin = math.ceil(max(galaxies[bin_feature]) * 10.0) / 10.0+bin_width
 
-bin_labels=np.arange(1, len(main_bins), 1)
-sub_bin_labels=np.arange(1, len(sub_bins), 1)
+    limite_inferior_sub_bin = math.floor(min(galaxies[sub_bin_feature]) * 10.0) / 10.0
+    limite_superior_sub_bin = math.ceil(max(galaxies[sub_bin_feature]) * 10.0) / 10.0+sub_bin_width
 
-galaxies[bin_feature+' bin'] = pd.cut(galaxies[bin_feature], main_bins, labels=bin_labels)
-galaxies[sub_bin_feature+' bin'] = pd.cut(galaxies[sub_bin_feature], sub_bins, labels=sub_bin_labels)
+    main_bins = np.arange(limite_inferior_main_bin, limite_superior_main_bin, bin_width)
+    sub_bins = np.arange(limite_inferior_sub_bin, limite_superior_sub_bin, sub_bin_width)
 
-halos[bin_feature+' bin'] = pd.cut(halos[bin_feature], main_bins, labels=bin_labels)
-halos[sub_bin_feature+' bin'] = pd.cut(halos[sub_bin_feature], sub_bins, labels=sub_bin_labels)
+    bin_labels=np.arange(1, len(main_bins), 1)
+    sub_bin_labels=np.arange(1, len(sub_bins), 1)
+
+    galaxies[bin_feature+' bin'] = pd.cut(galaxies[bin_feature], main_bins, labels=bin_labels)
+    galaxies[sub_bin_feature+' bin'] = pd.cut(galaxies[sub_bin_feature], sub_bins, labels=sub_bin_labels)
+
+    halos[bin_feature+' bin'] = pd.cut(halos[bin_feature], main_bins, labels=bin_labels)
+    halos[sub_bin_feature+' bin'] = pd.cut(halos[sub_bin_feature], sub_bins, labels=sub_bin_labels)
 
 
 
-galaxies.to_csv('Resultados/galaxies.csv', index=False)
+    galaxies.to_csv('Resultados/galaxies.csv', index=False)
 
-halos.to_csv('Resultados/halos.csv', index=False)
+    halos.to_csv('Resultados/halos.csv', index=False)
+    
+    return halos, galaxies, main_bins, sub_bins
--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import numpy as np
 import numpy.random as r
 import pandas as pd
 from Corrfunc.theory import xi
+
+from calculo_bins import calculo_bins
 from shuffling import galaxies_shuffle_bin, galaxies_shuffle, galaxies_shuffling_many
-from ploting_script imgalaxies_shuffleport ploting_results
+from ploting_script import ploting_results
 from calculo_2pcf import calculo_2pcf
+
 
 # Simulation parameters
 L = 205                  # Side length of simulation box
@@ -12,19 +15,29 @@ h = 0.6774               # Little Hubble constant
 
 # Analisys parameters
 corte_masa = 10.75       # We cut the galaxies by mass into a sample
-bin_width = 0.1          # Bin width in halo mass
 bin_number = 100         # Bin number in spatial bins (2PCF calculation)
 seed_number = 100        # We shuffle seed_number times and compute the mean and std
 n = 3                    # Number of sigmas in the plot
 n_threads = 1            # Number of threads to use to calculate the 2PCF
 
+bin_feature = 'Halo mass'
+sub_bin_feature = 'Halo mass squared'
 
-N_bins = len(np.arange(10.7, 15.2, bin_width))
+bin_width=0.1
+sub_bin_width=0.1
+
+
+
+# Calculation of original data
+import calculo_dataframe
+
+# Calculation of binned dataframes of halos and galaxies
+halos, galaxies, main_bins, sub_bins = calculo_bins(bin_feature, sub_bin_feature, bin_width, sub_bin_width)
+
+N_bins_feature = len(main_bins)
+N_bins_sub_feature = len(sub_bins)
 seed=np.arange(0, seed_number, 1)
 
-# We read the data
-halos = pd.read_csv('Resultados/halos.csv')
-galaxies = pd.read_csv('Resultados/galaxies.csv')
 
 # We get the sample by cutting in stellar mass
 galaxies_sample = galaxies[galaxies['Stellar mass']>corte_masa]
@@ -37,7 +50,7 @@ pcf_original.to_csv('Resultados/pcf_original.csv', index=False) # We save the or
 
 
 # Calculate the 2PCF shuffled
-lista_DataFrames = galaxies_shuffling_many(galaxies_sample, N_bins, seed) # We shuffle the galaxies into haloes of same mass bin
+lista_DataFrames = galaxies_shuffling_many(galaxies_sample, bin_feature, sub_bin_feature, N_bins_feature, N_bins_sub_feature, seed) # We shuffle the galaxies into haloes of same mass bin
 lista_xis = []
 for q in range(len(lista_DataFrames)):
     # We extract one DataFrame of shuffled galaxies
