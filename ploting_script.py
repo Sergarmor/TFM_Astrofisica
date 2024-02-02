ae1c732039392592bc6e2d47d46039f3485338dd
Sergarmor <108725112+Sergarmor@users.noreply.github.com>
2024-02-02 12:27:22 +0100

    Plots + Galaxies_shuffled saving

--- ploting_script.py
+++ ploting_script.py
@@ -1,44 +1,53 @@
-def ploting_results(pcf_original, b, sigma, n, L):
+def ploting_2pcf_ratio(pcf_original, pcf_shuffled, n, L, bin_feature, sub_bin_feature):
 
     import matplotlib.pyplot as plt
     import numpy as np
     import pandas as pd
 
-    # Here we plot everything
+    b = pcf_original['xi'] / pcf_shuffled_mean['mean'] # Assembly bias
+    sigma = pcf_original['xi']/pcf_shuffled_mean['mean'] * pcf_shuffled_mean['std'] # Assembly bias uncertainty
+    xerr = pcf_original['rmax'] - pcf_original['rmin']
+
 
     fig, ax = plt.subplots(1, 1, figsize=(6, 5))
-    ax.plot((pcf_original['ravg']), 
+
+    ax.errorbar((pcf_original['ravg']), 
             (b), 
-            color='C0', label='Data')
-    ax.fill_between((pcf_original['ravg']), 
-                    (b + n*sigma), 
-                    (b - n*sigma), 
-                    color='C1', label='Uncertainty', alpha=0.5)
-    ax.set_xlabel('ravg')
+            xerr = xerr,
+            yerr = n*sigma,
+            color='C1', label= bin_feature + ' and ' + sub_bin_feature + ' shuffle (10)', linestyle='--', marker='.', ecolor='C0')
+
+
+    ax.set_xlabel('Spatial scale [Mpc]')
     ax.set_ylabel(r'$\xi_{orig}$ / $\xi_{shuff}$')
-    ax.set_title('2PCF comparison')
+    ax.set_title('2PCF ratio')
+
     plt.vlines(L*0.1, -0.5, 2.5, linestyle='--', color='C3')
     ax.hlines(1, -(10), (100), linestyle='--', color='C3')
-    ax.set_ylim([-1, 2.5])
-    ax.set_xlim([0, (np.ceil(max(pcf_original['ravg'])))])
+
+    ax.set_ylim([-0.5, 2.5])
+    ax.set_xlim([0, ((max(pcf_original['ravg'])))])
     ax.legend()
     plt.savefig('Figuras/2pcf_ratio.png', bbox_inches='tight')
 
-
+    ##### ZOOM TO SMALL SCALES
     fig, ax = plt.subplots(1, 1, figsize=(6, 5))
+
     ax.plot(np.log10(pcf_original['ravg']), 
-            (b), 
-            color='C0', label='Data')
+                (b), 
+                color='C0', label= bin_feature + ' and ' + sub_bin_feature + ' shuffle (10)', linestyle='--', marker='.')
     ax.fill_between(np.log10(pcf_original['ravg']), 
-                    (b + n*sigma), 
-                    (b - n*sigma), 
-                    color='C1', label='Uncertainty', alpha=0.5)
-    ax.set_xlabel('log10(ravg)')
+                        (b + n*sigma), 
+                        (b - n*sigma), 
+                        color='C1', label='Uncertainty', alpha=0.5)
+    ax.set_xlabel(r'Spatial scale [$\log_{10}$(Mpc)]')
     ax.set_ylabel(r'$\xi_{orig}$ / $\xi_{shuff}$')
-    ax.set_title('2PCF small scale comparison')
+    ax.set_title('2PCF ratio: small scale')
+
     plt.vlines(L*0.1, -0.5, 2.5, linestyle='--', color='C3')
     ax.hlines(1, -(10), (100), linestyle='--', color='C3')
-    ax.set_ylim([0.5, 1.75])
+
+    ax.set_ylim([0.5, 1.5])
     ax.set_xlim([-1, np.log10(20)])
     ax.legend()
     plt.savefig('Figuras/2pcf_ratio_small_scale.png', bbox_inches='tight')
