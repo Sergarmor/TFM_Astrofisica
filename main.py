f13e49e9eeb5f8febc23aea701ec3c58a227c8bd
Sergarmor <108725112+Sergarmor@users.noreply.github.com>
2024-02-12 18:01:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ if rebin == 'yes':
     halos, galaxies = calculo_bins(halos, galaxies, bin_number)
 
 # We get the sample by cutting in stellar mass
-galaxies_sample = galaxies[galaxies['Stellar mass'] > corte_masa].copy()
+galaxies_sample = galaxies[galaxies.loc[:, 'Stellar mass'] > corte_masa].copy()
 
 # Calculate the original 2PCF
 
