74cf34211a48a392e1878eadea509967f1d94b36
Sergarmor <108725112+Sergarmor@users.noreply.github.com>
2024-02-12 18:25:13 +0100

    Update shuffling.py

--- shuffling.py
+++ shuffling.py
@@ -50,7 +50,7 @@ def galaxies_shuffle_optimized(halos, galaxies_sample, features, L):
             halos_bins = halos_bins[halos_bins[features_bins[p]] == int(bins.iloc[p])]
 
         ID_halo_nuevo = r.choice(halos_bins.loc[:, 'HaloID'], size=1, replace=False)[0]
-        halos_choose.drop(halos_choose['HaloID']==ID_halo_nuevo)
+        halos_choose.drop(halos_choose['HaloID']==ID_halo_nuevo, axis='index')
         
         
         halo_nuevo = halos_bins.loc[halos_bins['HaloID'] == ID_halo_nuevo]
