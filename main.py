87a7d159d506335b35bc55ce00a2de94c6b11dbc
Sergarmor <108725112+Sergarmor@users.noreply.github.com>
2024-02-14 15:22:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -76,8 +76,6 @@ rebin = input('Do you want to recalculate the binning of the DataFrames? [yes/no
 
 if rebin == 'yes':
     halos, galaxies = calculo_bins(halos, galaxies, bin_number)
-    
-    print(galaxies.columns)
 else:
     pass
     
