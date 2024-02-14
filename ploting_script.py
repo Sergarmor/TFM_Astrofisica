453915a46c0654923906dcb62acf70601f4c67c0
Sergarmor <108725112+Sergarmor@users.noreply.github.com>
2024-02-14 20:01:49 +0100

    Clearer plotting

--- ploting_script.py
+++ ploting_script.py
@@ -32,11 +32,22 @@ def ploting_2pcf_ratio(pcf_original, pcf_shuffled_mean, n, L, features, N_shuffl
     ##### ZOOM TO SMALL SCALES
     fig, ax = plt.subplots(1, 1, figsize=(6, 5))
 
-    ax.errorbar(np.log10(pcf_original['ravg']), 
+    # ax.errorbar(np.log10(pcf_original['ravg']), 
+            # (b), 
+            # xerr = abs(np.log10(xerr)),
+            # yerr = n*sigma,
+            # color='C0', label= f'Shuffle with features {features} ({N_shufflings})', linestyle='--', marker='.', ecolor='C1')
+            
+            
+    ax.plot(np.log10(pcf_original['ravg']), 
             (b), 
-            xerr = abs(np.log10(xerr)),
-            yerr = n*sigma,
-            color='C0', label= f'Shuffle with features {features} ({N_shufflings})', linestyle='--', marker='.', ecolor='C1')
+            color='C0', label='Data')
+    ax.fill_between(np.log10(pcf_original['ravg']), 
+                    (b + n*sigma), 
+                    (b - n*sigma), 
+                    color='C1', alpha=0.5, label='Uncertainty')
+                
+                
     ax.set_xlabel(r'Escala espacial [$\log_{10}$(Mpc)]')
     ax.set_ylabel(r'$\xi_{orig}$ / $\xi_{shuff}$')
     ax.set_title('2PCF ratio: small scale')
