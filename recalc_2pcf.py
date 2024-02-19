63a05ad11f235b46168bc25b246fffd6f395017b
Sergarmor <108725112+Sergarmor@users.noreply.github.com>
2024-02-19 10:52:57 +0100

    Update recalc_2pcf.py

--- recalc_2pcf.py
+++ recalc_2pcf.py
@@ -8,7 +8,14 @@ N_shufflings=100
 spatial_bin_number=25
 n_threads=1
 mass_cut=11.25
+shuffling_type=input('Which shuffle do you want? [1/2/3] meaning [mass/mass+concentration/mass+spin]: ')
 
+if shuffling_type == 1:
+    path = 'Halo_mass'
+elif shuffling_type == 2:
+    path = 'Halo_mass_concentration'
+elif shuffling_type == 3:
+    path = 'Halo_mass_spin'
 
 # We read the data
 galaxies = pd.read_csv('Resultados/galaxies.csv')
@@ -27,7 +34,7 @@ galaxies_list=[]
 
 for i in range(N_shufflings):
     # print(f'Shuffle number {i+1} out of {N_shufflings}')
-    galaxies_temp = pd.read_csv(f'Resultados/Shuffled/Galaxies/galaxies_shuffled{i}.csv')
+    galaxies_temp = pd.read_csv(f'Resultados/{path}/Shuffled/Galaxies/galaxies_shuffled{i}.csv')
     galaxies_list.append(galaxies_temp)
 
 lista_xis = []
@@ -39,7 +46,7 @@ for q in range(len(galaxies_list)):
     # We extract the 2PCF value of the shuffled galaxies (one iteration) and save it to use later
     pcf_shuffled = calculo_2pcf(galaxies_shuffled, L, spatial_bin_number, n_threads)
     
-    pcf_shuffled.to_csv(f'Resultados/Shuffled/PCF/pcf_shuffled{q}.csv', index=False) # We save the shuffled 2pcf
+    pcf_shuffled.to_csv(f'Resultados/{path}/Shuffled/PCF/pcf_shuffled{q}.csv', index=False) # We save the shuffled 2pcf
     
     
     pcf_shuffled_xi = pcf_shuffled['xi']
@@ -51,4 +58,4 @@ pcf_shuffled_xi = pd.concat(lista_xis, axis=1)
 pcf_shuffled_xi = pcf_shuffled_xi.assign(mean=pcf_shuffled_xi.mean(axis=1))
 pcf_shuffled_xi = pcf_shuffled_xi.assign(std=pcf_shuffled_xi.std(axis=1))
 pcf_shuffled_xi = pcf_shuffled_xi.loc[:, ['mean', 'std']] # We discard all the 2PCFs and maintain the mean and std. Then we save it
-pcf_shuffled_xi.to_csv('Resultados/pcf_shuffled_mean.csv', index=False)
+pcf_shuffled_xi.to_csv(f'Resultados/{path}/pcf_shuffled_mean.csv', index=False)
