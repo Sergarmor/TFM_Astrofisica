39d39f5c1e1f496b8668ac8831cf3297ff0dbf49
Sergarmor <108725112+Sergarmor@users.noreply.github.com>
2024-02-27 10:03:31 +0100

    Update recalc_2pcf.py

--- recalc_2pcf.py
+++ recalc_2pcf.py
@@ -4,10 +4,11 @@ import pandas as pd
 from calculo_2pcf import calculo_2pcf
 
 L=205
-N_shufflings=300
+N_shufflings=100
 spatial_bin_number=25
 n_threads=1
-mass_cut=float(input('Which mass do you want to cut at? Minnimun 10.75: '))
+mass_cut_1=float(input('Which mass do you want to cut at? Minnimun 10.75: '))
+mass_cut_2=float(input(f'Which mass do you want to cut at? Minnimun {mass_cut_1}: '))
 shuffling_type=int(input('Which shuffle do you want? [1/2/3] meaning [mass/mass+concentration/mass+spin]: '))
 
 if shuffling_type == 1:
@@ -17,15 +18,15 @@ elif shuffling_type == 2:
 elif shuffling_type == 3:
     path = 'Halo_mass_spin'
 
-if mass_cut < 10.75:
+if mass_cut_1 < 10.75:
     raise ValueError('Mass cut must be greater than 10.75')
 # We read the data
 galaxies = pd.read_csv('Resultados/galaxies.csv')
 
 
 # We get the sample by cutting in stellar mass
-galaxies_sample = galaxies[galaxies.loc[:, 'Stellar mass'] > mass_cut].copy()
-
+galaxies_sample = galaxies[galaxies.loc[:, 'Stellar mass'] > mass_cut_1].copy()
+galaxies_sample = galaxies_sample[galaxies_sample.loc[:, 'Stellar mass'] < mass_cut_2].copy()
 # Calculate the original 2PCF
 
 pcf_original = calculo_2pcf(galaxies_sample, L, spatial_bin_number, n_threads)
@@ -60,4 +61,4 @@ pcf_shuffled_xi = pd.concat(lista_xis, axis=1)
 pcf_shuffled_xi = pcf_shuffled_xi.assign(mean=pcf_shuffled_xi.mean(axis=1))
 pcf_shuffled_xi = pcf_shuffled_xi.assign(std=pcf_shuffled_xi.std(axis=1))
 pcf_shuffled_xi = pcf_shuffled_xi.loc[:, ['mean', 'std']] # We discard all the 2PCFs and maintain the mean and std. Then we save it
-pcf_shuffled_xi.to_csv(f'Resultados/{path}/pcf_shuffled_mean.csv', index=False)
+pcf_shuffled_xi.to_csv(f'Resultados/{path}/Double_cut/pcf_shuffled_mean.csv', index=False)
