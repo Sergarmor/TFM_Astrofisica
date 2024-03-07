2448a2c71150374ef80b7d0e2e9d5bcbb7320136
Sergarmor <108725112+Sergarmor@users.noreply.github.com>
2024-03-07 13:52:32 +0100

    Revert "Update recalc_2pcf.py"
    This reverts commit b93c2aad68cb5429ace4dd081686a82595b3d89a.

--- recalc_2pcf.py
+++ recalc_2pcf.py
@@ -7,7 +7,7 @@ L=205
 N_shufflings=100
 spatial_bin_number=25
 n_threads=1
-mass_cut_1=float(input('Which mass do you want to cut at? Minnimun 10.5: '))
+mass_cut_1=float(input('Which mass do you want to cut at? Minnimun 10.75: '))
 mass_cut_2=float(input(f'Which mass do you want to cut at? Minnimun {mass_cut_1}: '))
 shuffling_type=int(input('Which shuffle do you want? [1/2/3] meaning [mass/mass+concentration/mass+spin]: '))
 
@@ -18,8 +18,8 @@ elif shuffling_type == 2:
 elif shuffling_type == 3:
     path = 'Halo_mass_spin'
 
-if mass_cut_1 < 10.5:
-    raise ValueError('Mass cut must be greater than 10.5')
+if mass_cut_1 < 10.75:
+    raise ValueError('Mass cut must be greater than 10.75')
 # We read the data
 galaxies = pd.read_csv('Resultados/galaxies.csv')
 
@@ -31,7 +31,7 @@ galaxies_sample = galaxies_sample[galaxies_sample.loc[:, 'Stellar mass'] < mass_
 
 pcf_original = calculo_2pcf(galaxies_sample, L, spatial_bin_number, n_threads)
 
-pcf_original.to_csv('Resultados/2pcf_recalc/pcf_original.csv', index=False) # We save the original 2PCF
+pcf_original.to_csv('Resultados/pcf_original.csv', index=False) # We save the original 2PCF
 
 galaxies_list=[]
 
@@ -50,7 +50,7 @@ for q in range(len(galaxies_list)):
     # We extract the 2PCF value of the shuffled galaxies (one iteration) and save it to use later
     pcf_shuffled = calculo_2pcf(galaxies_shuffled, L, spatial_bin_number, n_threads)
     
-    pcf_shuffled.to_csv(f'Resultados/2pcf_recalc/{path}/Shuffled/pcf_shuffled{q}.csv', index=False) # We save the shuffled 2pcf
+    pcf_shuffled.to_csv(f'Resultados/{path}/Shuffled/PCF/pcf_shuffled{q}.csv', index=False) # We save the shuffled 2pcf
     
     
     pcf_shuffled_xi = pcf_shuffled['xi']
@@ -62,4 +62,4 @@ pcf_shuffled_xi = pd.concat(lista_xis, axis=1)
 pcf_shuffled_xi = pcf_shuffled_xi.assign(mean=pcf_shuffled_xi.mean(axis=1))
 pcf_shuffled_xi = pcf_shuffled_xi.assign(std=pcf_shuffled_xi.std(axis=1))
 pcf_shuffled_xi = pcf_shuffled_xi.loc[:, ['mean', 'std']] # We discard all the 2PCFs and maintain the mean and std. Then we save it
-pcf_shuffled_xi.to_csv(f'Resultados/2pcf_recalc/{path}/pcf_shuffled_mean_{mass_cut_1}_{mass_cut_2}.csv', index=False)
+pcf_shuffled_xi.to_csv(f'Resultados/{path}/Double_cut/pcf_shuffled_mean.csv', index=False)
