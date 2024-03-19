61c15d9f8a2b283e2499e00a9397dd86f8893a3a
Sergarmor <108725112+Sergarmor@users.noreply.github.com>
2024-03-19 16:55:20 +0100

    Update halo_pcf.py

--- halo_pcf.py
+++ halo_pcf.py
@@ -1,3 +1,48 @@
+def calculo_2pcf(galaxies, L, bin_number, n_threads):
+
+    """
+    Función que calcula la 2PCF a partir de un set de galaxias.
+    
+    Parameters
+    ----------
+    galaxies : DataFrame
+              Galaxias. Debe contener las columnas 'Pos_x/y/z'.
+    L : float
+        Anchura de caja.
+    bin_number : int
+                 Número de bins espaciales.
+    n_threads : int
+                Número de threads a usar en el cálculo de la 2PCF.
+                
+    Returns
+    -------
+    pcf : DataFrame
+          DataFrame conteniendo la información de la 2PCF frente a escala
+    """
+    
+    import numpy as np
+    import pandas as pd
+    from Corrfunc.theory import xi
+    
+    # Coordenadas de las galaxias
+    X = galaxies['Halo_x']
+    Y = galaxies['Halo_y']
+    Z = galaxies['Halo_z']
+    
+    # Parámetros del cálculo de la 2PCF
+    weights = np.ones_like(X)
+    limit_spatial_bins = np.log10(25)
+    spatial_bins = np.logspace(np.log10(0.1), limit_spatial_bins, bin_number+1)
+
+    # Cálculo de la 2PCF
+    pcf = xi(L, n_threads, spatial_bins, X, Y, Z, weights = weights)
+    pcf = pd.DataFrame(pcf, columns=['rmin', 'rmax', 'ravg', 'xi', 'npairs', 'weightavg'])
+    
+    pcf['ravg'] = (pcf['rmax'] + pcf['rmin'])/2
+    
+    return pcf
+
+
 import numpy as np
 import pandas as pd
 from Corrfunc.theory import xi
