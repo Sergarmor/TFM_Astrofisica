b987dc6605a4ac97026dd47a2bc74e9bc2e58148
Sergio <parasergiogarcia@gmail.com>
2024-03-28 10:43:09 +0100

    vrank3 run

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ h = 0.6774                                   # Little Hubble constant
 mass_cut = 10.5                           # We cut the galaxies by mass into a sample
 
 # Features used to do the binning and shuffle the galaxies
-features=['Halo mass', 'Halo vrank 2']
+features=['Halo mass', 'Halo vrank 3']
 
 # new_feature = input('Which feature do you want to add? Press 0 to add none: ')
 N_shufflings = 20
