0eed2522c79e53956c69a5942691a1c6ad32e7f4
Sergio <parasergiogarcia@gmail.com>
2024-04-10 13:18:53 +0200

    Data Frame split up

--- calculo_dataframe.py
+++ calculo_dataframe.py
@@ -6,12 +6,16 @@ file_extra = np.load('Datos/extra_prop_v0.npy', allow_pickle=True).item()
 file_extra_1 = np.load('Datos/extra_prop_v1.npy', allow_pickle=True).item()
 
 # Building Halo DataFrame
+
+# Necesary properties for all parts
 halo_id = file['halo_id']
 halo_mass = file['halo_mass']
-halo_conc = file['halo_conc']
-halo_spin = file['halo_spin']
 halo_pos = file['halo_pos']
 halo_vel = file['halo_vel']
+
+halo_conc = file['halo_conc']
+halo_spin = file['halo_spin']
+
 halo_mrank = file_extra['mrank']
 halo_vrank = file_extra['vrank']
 halo_tmform = file_extra['tmform']
@@ -22,18 +26,44 @@ halo_vrank_1 = file_extra_1['vrank']
 halo_tmform_1 = file_extra_1['tmform']
 
 # Didn't use
-halo_mhist = file['halo_mhist']
-halo_vhist = file['halo_vhist']
+# halo_mhist = file['halo_mhist']
+# halo_vhist = file['halo_vhist']
 
-halos = pd.DataFrame(
+halos_part1 = pd.DataFrame(
     data=np.array([halo_id, 
                    halo_mass, 
                    halo_conc, 
                    halo_spin, 
+                   halo_pos[:, 0], halo_pos[:, 1], halo_pos[:, 2], 
+                   halo_vel[:, 0], halo_vel[:, 1], halo_vel[:, 2]]).transpose(), 
+    columns=['HaloID', 
+    'Halo mass', 
+    'Halo concentration', 
+    'Halo spin', 
+    'x', 'y', 'z', 
+    'Vel_x', 'Vel_y', 'Vel_z'])
+
+halos_part2 = pd.DataFrame(
+    data=np.array([halo_id, 
+                   halo_mass, 
                    halo_mrank[:, 0], halo_mrank[:, 1], halo_mrank[:, 2], 
                    halo_mrank_1[:, 0], halo_mrank_1[:, 1], halo_mrank_1[:, 2], 
                    halo_vrank[:, 0], halo_vrank[:, 1], halo_vrank[:, 2], 
                    halo_vrank_1[:, 0], halo_vrank_1[:, 1], halo_vrank_1[:, 2], 
+                   halo_pos[:, 0], halo_pos[:, 1], halo_pos[:, 2], 
+                   halo_vel[:, 0], halo_vel[:, 1], halo_vel[:, 2]]).transpose(), 
+    columns=['HaloID', 
+    'Halo mass', 
+    'Halo mrank 1', 'Halo mrank 2', 'Halo mrank 3', 
+    'Halo mrank 4', 'Halo mrank 5', 'Halo mrank 6', 
+    'Halo vrank 1', 'Halo vrank 2', 'Halo vrank 3', 
+    'Halo vrank 4', 'Halo vrank 5', 'Halo vrank 6', 
+    'x', 'y', 'z', 
+    'Vel_x', 'Vel_y', 'Vel_z'])
+
+halos_part3 = pd.DataFrame(
+    data=np.array([halo_id, 
+                   halo_mass, 
                    halo_tmform[:, 0], halo_tmform[:, 1], halo_tmform[:, 2], 
                    halo_tmform_1[:, 0], halo_tmform_1[:, 1],
                    halo_tvform[:, 0], halo_tvform[:, 1], halo_tvform[:, 2], 
@@ -41,22 +71,51 @@ halos = pd.DataFrame(
                    halo_vel[:, 0], halo_vel[:, 1], halo_vel[:, 2]]).transpose(), 
     columns=['HaloID', 
     'Halo mass', 
-    'Halo concentration', 
-    'Halo spin', 
-    'Halo mrank 1', 'Halo mrank 2', 'Halo mrank 3', 
-    'Halo mrank 4', 'Halo mrank 5', 'Halo mrank 6', 
-    'Halo vrank 1', 'Halo vrank 2', 'Halo vrank 3', 
-    'Halo vrank 4', 'Halo vrank 5', 'Halo vrank 6', 
     'Halo tmform 1', 'Halo tmform 2', 'Halo tmform 3', 
     'Halo tmform 4', 'Halo tmform 5',
     'Halo tvform 1', 'Halo tvform 2', 'Halo tvform 3',
     'x', 'y', 'z', 
     'Vel_x', 'Vel_y', 'Vel_z'])
-halos['HaloID'] = halos['HaloID'].astype(int) # We force IDs to be integers
 
-halos = halos.sort_values(by='HaloID')
+
+# halos = pd.DataFrame(
+#     data=np.array([halo_id, 
+#                    halo_mass, 
+#                    halo_conc, 
+#                    halo_spin, 
+#                    halo_mrank[:, 0], halo_mrank[:, 1], halo_mrank[:, 2], 
+#                    halo_mrank_1[:, 0], halo_mrank_1[:, 1], halo_mrank_1[:, 2], 
+#                    halo_vrank[:, 0], halo_vrank[:, 1], halo_vrank[:, 2], 
+#                    halo_vrank_1[:, 0], halo_vrank_1[:, 1], halo_vrank_1[:, 2], 
+#                    halo_tmform[:, 0], halo_tmform[:, 1], halo_tmform[:, 2], 
+#                    halo_tmform_1[:, 0], halo_tmform_1[:, 1],
+#                    halo_tvform[:, 0], halo_tvform[:, 1], halo_tvform[:, 2], 
+#                    halo_pos[:, 0], halo_pos[:, 1], halo_pos[:, 2], 
+#                    halo_vel[:, 0], halo_vel[:, 1], halo_vel[:, 2]]).transpose(), 
+#     columns=['HaloID', 
+#     'Halo mass', 
+#     'Halo concentration', 
+#     'Halo spin', 
+#     'Halo mrank 1', 'Halo mrank 2', 'Halo mrank 3', 
+#     'Halo mrank 4', 'Halo mrank 5', 'Halo mrank 6', 
+#     'Halo vrank 1', 'Halo vrank 2', 'Halo vrank 3', 
+#     'Halo vrank 4', 'Halo vrank 5', 'Halo vrank 6', 
+#     'Halo tmform 1', 'Halo tmform 2', 'Halo tmform 3', 
+#     'Halo tmform 4', 'Halo tmform 5',
+#     'Halo tvform 1', 'Halo tvform 2', 'Halo tvform 3',
+#     'x', 'y', 'z', 
+#     'Vel_x', 'Vel_y', 'Vel_z'])
+
+halos_part1['HaloID'] = halos_part1['HaloID'].astype(int) # We force IDs to be integers
+halos_part2['HaloID'] = halos_part2['HaloID'].astype(int) # We force IDs to be integers
+halos_part3['HaloID'] = halos_part3['HaloID'].astype(int) # We force IDs to be integers
+
+halos_part1 = halos_part1.sort_values(by='HaloID')
+halos_part2 = halos_part2.sort_values(by='HaloID')
+halos_part3 = halos_part3.sort_values(by='HaloID')
 
 # Building Galaxies DataFrame
+## Necesary properties
 gal_hostid = file['idhost']
 gal_cross = file['cross_sub2halo']
 gal_type = file['type'] # Guide: 0 --> Central. | 1 --> Satellite
@@ -105,6 +164,38 @@ columns = ['HostID',
            'Vel_x', 'Vel_y', 'Vel_z',
            'COP_vel_x', 'COP_vel_y', 'COP_vel_z']
 
+galaxies_part1 = pd.DataFrame(
+    data = np.array([gal_hostid, 
+                    gal_cross,
+                    gal_type, 
+                    gal_mst, 
+                    gal_halo_mass, 
+                    gal_halo_conc,
+                    gal_halo_spin,
+                    gal_halo_pos[:, 0], gal_halo_pos[:, 1], gal_halo_pos[:, 2],
+                    gal_halo_vel[:, 0], gal_halo_vel[:, 1], gal_halo_vel[:, 2], 
+                    gal_col[:, 0], gal_col[:, 1], gal_col[:, 2], 
+                    gal_pos[:, 0], gal_pos[:, 1], gal_pos[:, 2], 
+                    gal_pos[:, 0] - gal_halo_pos[:, 0], gal_pos[:, 1] - gal_halo_pos[:, 1], gal_pos[:, 2] - gal_halo_pos[:, 2],
+                    gal_vel[:, 0], gal_vel[:, 1], gal_vel[:, 2],
+                    gal_vel[:, 0] - gal_halo_vel[:, 0], gal_vel[:, 1] - gal_halo_vel[:, 1], gal_vel[:, 2] - gal_halo_vel[:, 2],
+                    ]).transpose(),
+    columns=['HostID', 
+           'Host index', 
+           'Type', 
+           'Stellar mass', 
+           'Halo mass', 
+           'Halo concentration', 
+           'Halo spin', 
+           'Halo_x', 'Halo_y', 'Halo_z', 
+           'Halo_vel_x', 'Halo_vel_y', 'Halo_vel_z', 
+           'Col_x', 'Col_y', 'Col_z', 
+           'Pos_x', 'Pos_y', 'Pos_z',
+           'COP_x', 'COP_y', 'COP_z',
+           'Vel_x', 'Vel_y', 'Vel_z',
+           'COP_vel_x', 'COP_vel_y', 'COP_vel_z']
+)
+
 galaxies = pd.DataFrame(
     data=np.array([gal_hostid, 
                    gal_cross,
@@ -139,4 +230,8 @@ galaxies['Type'] = galaxies['Type'].astype(int)
 galaxies = galaxies.sort_values(by='HostID')
 
 galaxies.to_csv('Resultados/galaxies.csv', index=False)
-halos.to_csv('Resultados/halos.csv', index=False)
+
+# Save halos DataFrames
+halos_part1.to_csv('Resultados/halos_part1.csv', index=False)
+halos_part2.to_csv('Resultados/halos_part2.csv', index=False)
+halos_part3.to_csv('Resultados/halos_part3.csv', index=False)
