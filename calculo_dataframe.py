6350488b8c278e1654fffb445d522edfc30f23aa
Sergio <parasergiogarcia@gmail.com>
2024-04-10 09:47:13 +0200

    Updated galaxy data frame

--- calculo_dataframe.py
+++ calculo_dataframe.py
@@ -73,6 +73,11 @@ gal_halo_spin = file['halo_spin'][file['cross_sub2halo']]
 gal_halo_mrank = file_extra['mrank'][file['cross_sub2halo']]
 gal_halo_vrank = file_extra['vrank'][file['cross_sub2halo']]
 gal_halo_tmform = file_extra['tmform'][file['cross_sub2halo']]
+
+gal_halo_mrank_1 = file_extra_1['mrank'][file['cross_sub2halo']]
+gal_halo_vrank_1 = file_extra_1['vrank'][file['cross_sub2halo']]
+gal_halo_tmform_1 = file_extra_1['tmform'][file['cross_sub2halo']]
+
 gal_halo_tvform = file_extra['tvform'][file['cross_sub2halo']]
 gal_halo_pos = file['halo_pos'][file['cross_sub2halo']]
 gal_halo_vel = file['halo_vel'][file['cross_sub2halo']]
@@ -86,8 +91,11 @@ columns = ['HostID',
            'Halo concentration', 
            'Halo spin', 
            'Halo mrank 1', 'Halo mrank 2', 'Halo mrank 3', 
+           'Halo mrank 4', 'Halo mrank 5', 'Halo mrank 6', 
            'Halo vrank 1', 'Halo vrank 2', 'Halo vrank 3', 
+           'Halo vrank 4', 'Halo vrank 5', 'Halo vrank 6', 
            'Halo tmform 1', 'Halo tmform 2', 'Halo tmform 3', 
+           'Halo tmform 4', 'Halo tmform 5',
            'Halo tvform 1', 'Halo tvform 2', 'Halo tvform 3',
            'Halo_x', 'Halo_y', 'Halo_z', 
            'Halo_vel_x', 'Halo_vel_y', 'Halo_vel_z', 
@@ -106,8 +114,11 @@ galaxies = pd.DataFrame(
                    gal_halo_conc, 
                    gal_halo_spin,
                    gal_halo_mrank[:, 0], gal_halo_mrank[:, 1], gal_halo_mrank[:, 2],
+                   gal_halo_mrank_1[:, 0], gal_halo_mrank_1[:, 1], gal_halo_mrank_1[:, 2],
                    gal_halo_vrank[:, 0], gal_halo_vrank[:, 1], gal_halo_vrank[:, 2],
+                   gal_halo_vrank_1[:, 0], gal_halo_vrank_1[:, 1], gal_halo_vrank_1[:, 2],
                    gal_halo_tmform[:, 0], gal_halo_tmform[:, 1], gal_halo_tmform[:, 2],
+                   gal_halo_tmform_1[:, 0], gal_halo_tmform_1[:, 1], 
                    gal_halo_tvform[:, 0], gal_halo_tvform[:, 1], gal_halo_tvform[:, 2],                   
                    gal_halo_pos[:, 0], gal_halo_pos[:, 1], gal_halo_pos[:, 2],
                    gal_halo_vel[:, 0], gal_halo_vel[:, 1], gal_halo_vel[:, 2], 
