4c54d8ca249af24951df3d638eee2b314a80084a
Sergio <parasergiogarcia@gmail.com>
2024-04-11 20:38:27 +0200

    Data re-structuring complete
    DataFrames divided in three separate files for easier management.

--- calculo_dataframe.py
+++ calculo_dataframe.py
@@ -110,6 +110,7 @@ halos_part1['HaloID'] = halos_part1['HaloID'].astype(int) # We force IDs to be i
 halos_part2['HaloID'] = halos_part2['HaloID'].astype(int) # We force IDs to be integers
 halos_part3['HaloID'] = halos_part3['HaloID'].astype(int) # We force IDs to be integers
 
+# Save halos DataFrames
 halos_part1 = halos_part1.sort_values(by='HaloID')
 halos_part2 = halos_part2.sort_values(by='HaloID')
 halos_part3 = halos_part3.sort_values(by='HaloID')
@@ -142,27 +143,27 @@ gal_halo_pos = file['halo_pos'][file['cross_sub2halo']]
 gal_halo_vel = file['halo_vel'][file['cross_sub2halo']]
 
 
-columns = ['HostID', 
-           'Host index', 
-           'Type', 
-           'Stellar mass', 
-           'Halo mass', 
-           'Halo concentration', 
-           'Halo spin', 
-           'Halo mrank 1', 'Halo mrank 2', 'Halo mrank 3', 
-           'Halo mrank 4', 'Halo mrank 5', 'Halo mrank 6', 
-           'Halo vrank 1', 'Halo vrank 2', 'Halo vrank 3', 
-           'Halo vrank 4', 'Halo vrank 5', 'Halo vrank 6', 
-           'Halo tmform 1', 'Halo tmform 2', 'Halo tmform 3', 
-           'Halo tmform 4', 'Halo tmform 5',
-           'Halo tvform 1', 'Halo tvform 2', 'Halo tvform 3',
-           'Halo_x', 'Halo_y', 'Halo_z', 
-           'Halo_vel_x', 'Halo_vel_y', 'Halo_vel_z', 
-           'Col_x', 'Col_y', 'Col_z', 
-           'Pos_x', 'Pos_y', 'Pos_z',
-           'COP_x', 'COP_y', 'COP_z',
-           'Vel_x', 'Vel_y', 'Vel_z',
-           'COP_vel_x', 'COP_vel_y', 'COP_vel_z']
+# columns = ['HostID', 
+#            'Host index', 
+#            'Type', 
+#            'Stellar mass', 
+#            'Halo mass', 
+#            'Halo concentration', 
+#            'Halo spin', 
+#            'Halo mrank 1', 'Halo mrank 2', 'Halo mrank 3', 
+#            'Halo mrank 4', 'Halo mrank 5', 'Halo mrank 6', 
+#            'Halo vrank 1', 'Halo vrank 2', 'Halo vrank 3', 
+#            'Halo vrank 4', 'Halo vrank 5', 'Halo vrank 6', 
+#            'Halo tmform 1', 'Halo tmform 2', 'Halo tmform 3', 
+#            'Halo tmform 4', 'Halo tmform 5',
+#            'Halo tvform 1', 'Halo tvform 2', 'Halo tvform 3',
+#            'Halo_x', 'Halo_y', 'Halo_z', 
+#            'Halo_vel_x', 'Halo_vel_y', 'Halo_vel_z', 
+#            'Col_x', 'Col_y', 'Col_z', 
+#            'Pos_x', 'Pos_y', 'Pos_z',
+#            'COP_x', 'COP_y', 'COP_z',
+#            'Vel_x', 'Vel_y', 'Vel_z',
+#            'COP_vel_x', 'COP_vel_y', 'COP_vel_z']
 
 galaxies_part1 = pd.DataFrame(
     data = np.array([gal_hostid, 
@@ -196,40 +197,124 @@ galaxies_part1 = pd.DataFrame(
            'COP_vel_x', 'COP_vel_y', 'COP_vel_z']
 )
 
-galaxies = pd.DataFrame(
-    data=np.array([gal_hostid, 
-                   gal_cross,
-                   gal_type, 
-                   gal_mst, 
-                   gal_halo_mass, 
-                   gal_halo_conc, 
-                   gal_halo_spin,
-                   gal_halo_mrank[:, 0], gal_halo_mrank[:, 1], gal_halo_mrank[:, 2],
-                   gal_halo_mrank_1[:, 0], gal_halo_mrank_1[:, 1], gal_halo_mrank_1[:, 2],
-                   gal_halo_vrank[:, 0], gal_halo_vrank[:, 1], gal_halo_vrank[:, 2],
-                   gal_halo_vrank_1[:, 0], gal_halo_vrank_1[:, 1], gal_halo_vrank_1[:, 2],
-                   gal_halo_tmform[:, 0], gal_halo_tmform[:, 1], gal_halo_tmform[:, 2],
-                   gal_halo_tmform_1[:, 0], gal_halo_tmform_1[:, 1], 
-                   gal_halo_tvform[:, 0], gal_halo_tvform[:, 1], gal_halo_tvform[:, 2],                   
-                   gal_halo_pos[:, 0], gal_halo_pos[:, 1], gal_halo_pos[:, 2],
-                   gal_halo_vel[:, 0], gal_halo_vel[:, 1], gal_halo_vel[:, 2], 
-                   gal_col[:, 0], gal_col[:, 1], gal_col[:, 2], 
-                   gal_pos[:, 0], gal_pos[:, 1], gal_pos[:, 2], 
-                   gal_pos[:, 0] - gal_halo_pos[:, 0], gal_pos[:, 1] - gal_halo_pos[:, 1], gal_pos[:, 2] - gal_halo_pos[:, 2],
-                   gal_vel[:, 0], gal_vel[:, 1], gal_vel[:, 2],
-                   gal_vel[:, 0] - gal_halo_vel[:, 0], gal_vel[:, 1] - gal_halo_vel[:, 1], gal_vel[:, 2] - gal_halo_vel[:, 2],
-                   ]).transpose(),
-    columns=columns)
+galaxies_part2 = pd.DataFrame(
+    data = np.array([gal_hostid, 
+                    gal_cross,
+                    gal_type, 
+                    gal_mst, 
+                    gal_halo_mass, 
+                    gal_halo_mrank[:, 0], gal_halo_mrank[:, 1], gal_halo_mrank[:, 2],
+                    gal_halo_mrank_1[:, 0], gal_halo_mrank_1[:, 1], gal_halo_mrank_1[:, 2],
+                    gal_halo_vrank[:, 0], gal_halo_vrank[:, 1], gal_halo_vrank[:, 2],
+                    gal_halo_vrank_1[:, 0], gal_halo_vrank_1[:, 1], gal_halo_vrank_1[:, 2],
+                    gal_halo_pos[:, 0], gal_halo_pos[:, 1], gal_halo_pos[:, 2],
+                    gal_halo_vel[:, 0], gal_halo_vel[:, 1], gal_halo_vel[:, 2], 
+                    gal_col[:, 0], gal_col[:, 1], gal_col[:, 2], 
+                    gal_pos[:, 0], gal_pos[:, 1], gal_pos[:, 2], 
+                    gal_pos[:, 0] - gal_halo_pos[:, 0], gal_pos[:, 1] - gal_halo_pos[:, 1], gal_pos[:, 2] - gal_halo_pos[:, 2],
+                    gal_vel[:, 0], gal_vel[:, 1], gal_vel[:, 2],
+                    gal_vel[:, 0] - gal_halo_vel[:, 0], gal_vel[:, 1] - gal_halo_vel[:, 1], gal_vel[:, 2] - gal_halo_vel[:, 2],
+                    ]).transpose(),
+    columns=['HostID', 
+           'Host index', 
+           'Type', 
+           'Stellar mass', 
+           'Halo mass', 
+           'Halo mrank 1', 'Halo mrank 2', 'Halo mrank 3', 
+           'Halo mrank 4', 'Halo mrank 5', 'Halo mrank 6', 
+           'Halo vrank 1', 'Halo vrank 2', 'Halo vrank 3', 
+           'Halo vrank 4', 'Halo vrank 5', 'Halo vrank 6', 
+           'Halo_x', 'Halo_y', 'Halo_z', 
+           'Halo_vel_x', 'Halo_vel_y', 'Halo_vel_z', 
+           'Col_x', 'Col_y', 'Col_z', 
+           'Pos_x', 'Pos_y', 'Pos_z',
+           'COP_x', 'COP_y', 'COP_z',
+           'Vel_x', 'Vel_y', 'Vel_z',
+           'COP_vel_x', 'COP_vel_y', 'COP_vel_z']
+)
+
+galaxies_part3 = pd.DataFrame(
+    data = np.array([gal_hostid, 
+                    gal_cross,
+                    gal_type, 
+                    gal_mst, 
+                    gal_halo_mass, 
+                    gal_halo_tmform[:, 0], gal_halo_tmform[:, 1], gal_halo_tmform[:, 2],
+                    gal_halo_tmform_1[:, 0], gal_halo_tmform_1[:, 1], 
+                    gal_halo_tvform[:, 0], gal_halo_tvform[:, 1], gal_halo_tvform[:, 2],
+                    gal_halo_pos[:, 0], gal_halo_pos[:, 1], gal_halo_pos[:, 2],
+                    gal_halo_vel[:, 0], gal_halo_vel[:, 1], gal_halo_vel[:, 2], 
+                    gal_col[:, 0], gal_col[:, 1], gal_col[:, 2], 
+                    gal_pos[:, 0], gal_pos[:, 1], gal_pos[:, 2], 
+                    gal_pos[:, 0] - gal_halo_pos[:, 0], gal_pos[:, 1] - gal_halo_pos[:, 1], gal_pos[:, 2] - gal_halo_pos[:, 2],
+                    gal_vel[:, 0], gal_vel[:, 1], gal_vel[:, 2],
+                    gal_vel[:, 0] - gal_halo_vel[:, 0], gal_vel[:, 1] - gal_halo_vel[:, 1], gal_vel[:, 2] - gal_halo_vel[:, 2],
+                    ]).transpose(),
+    columns=['HostID', 
+           'Host index', 
+           'Type', 
+           'Stellar mass', 
+           'Halo mass', 
+           'Halo tmform 1', 'Halo tmform 2', 'Halo tmform 3', 
+           'Halo tmform 4', 'Halo tmform 5',
+           'Halo tvform 1', 'Halo tvform 2', 'Halo tvform 3',
+           'Halo_x', 'Halo_y', 'Halo_z', 
+           'Halo_vel_x', 'Halo_vel_y', 'Halo_vel_z', 
+           'Col_x', 'Col_y', 'Col_z', 
+           'Pos_x', 'Pos_y', 'Pos_z',
+           'COP_x', 'COP_y', 'COP_z',
+           'Vel_x', 'Vel_y', 'Vel_z',
+           'COP_vel_x', 'COP_vel_y', 'COP_vel_z']
+)
+
+# galaxies = pd.DataFrame(
+#     data=np.array([gal_hostid, 
+#                    gal_cross,
+#                    gal_type, 
+#                    gal_mst, 
+#                    gal_halo_mass, 
+#                    gal_halo_conc, 
+#                    gal_halo_spin,
+#                    gal_halo_mrank[:, 0], gal_halo_mrank[:, 1], gal_halo_mrank[:, 2],
+#                    gal_halo_mrank_1[:, 0], gal_halo_mrank_1[:, 1], gal_halo_mrank_1[:, 2],
+#                    gal_halo_vrank[:, 0], gal_halo_vrank[:, 1], gal_halo_vrank[:, 2],
+#                    gal_halo_vrank_1[:, 0], gal_halo_vrank_1[:, 1], gal_halo_vrank_1[:, 2],
+#                    gal_halo_tmform[:, 0], gal_halo_tmform[:, 1], gal_halo_tmform[:, 2],
+#                    gal_halo_tmform_1[:, 0], gal_halo_tmform_1[:, 1], 
+#                    gal_halo_tvform[:, 0], gal_halo_tvform[:, 1], gal_halo_tvform[:, 2],                   
+#                    gal_halo_pos[:, 0], gal_halo_pos[:, 1], gal_halo_pos[:, 2],
+#                    gal_halo_vel[:, 0], gal_halo_vel[:, 1], gal_halo_vel[:, 2], 
+#                    gal_col[:, 0], gal_col[:, 1], gal_col[:, 2], 
+#                    gal_pos[:, 0], gal_pos[:, 1], gal_pos[:, 2], 
+#                    gal_pos[:, 0] - gal_halo_pos[:, 0], gal_pos[:, 1] - gal_halo_pos[:, 1], gal_pos[:, 2] - gal_halo_pos[:, 2],
+#                    gal_vel[:, 0], gal_vel[:, 1], gal_vel[:, 2],
+#                    gal_vel[:, 0] - gal_halo_vel[:, 0], gal_vel[:, 1] - gal_halo_vel[:, 1], gal_vel[:, 2] - gal_halo_vel[:, 2],
+#                    ]).transpose(),
+#     columns=columns)
     
 
 # We force columns to be integers
-galaxies['HostID'] = galaxies['HostID'].astype(int)
-galaxies['Host index'] = galaxies['Host index'].astype(int)
-galaxies['Type'] = galaxies['Type'].astype(int)
+galaxies_part1['HostID'] = galaxies_part1['HostID'].astype(int)
+galaxies_part1['Host index'] = galaxies_part1['Host index'].astype(int)
+galaxies_part1['Type'] = galaxies_part1['Type'].astype(int)
 
-galaxies = galaxies.sort_values(by='HostID')
+galaxies_part2['HostID'] = galaxies_part2['HostID'].astype(int)
+galaxies_part2['Host index'] = galaxies_part2['Host index'].astype(int)
+galaxies_part2['Type'] = galaxies_part2['Type'].astype(int)
 
-galaxies.to_csv('Resultados/galaxies.csv', index=False)
+galaxies_part3['HostID'] = galaxies_part3['HostID'].astype(int)
+galaxies_part3['Host index'] = galaxies_part3['Host index'].astype(int)
+galaxies_part3['Type'] = galaxies_part3['Type'].astype(int)
+
+# Sort galaxies by HostID
+galaxies_part1 = galaxies_part1.sort_values(by='HostID')
+galaxies_part2 = galaxies_part2.sort_values(by='HostID')
+galaxies_part3 = galaxies_part3.sort_values(by='HostID')
+
+# Save galaxy DataFrames
+galaxies_part1.to_csv('Resultados/galaxies_part1.csv', index=False)
+galaxies_part2.to_csv('Resultados/galaxies_part2.csv', index=False)
+galaxies_part3.to_csv('Resultados/galaxies_part3.csv', index=False)
 
 # Save halos DataFrames
 halos_part1.to_csv('Resultados/halos_part1.csv', index=False)
