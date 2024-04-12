69d8caf263588aa6792a1f52b4cdfe80f0f0ed16
Sergio <parasergiogarcia@gmail.com>
2024-04-12 17:32:00 +0200

    Remerged main script
    It still needs to be executed several times for each property

--- main.py
+++ main.py
@@ -142,6 +142,28 @@ elif file_part == 'part3':
 else:
     raise KeyError(file_part)
 
+lista_xis = []
+for q in range(len(lista_DataFrames)):
+    # We extract one DataFrame of shuffled galaxies
+    galaxies_shuffled = lista_DataFrames[q]
+    
+    # We extract the 2PCF value of the shuffled galaxies (one iteration) and save it to use later
+    pcf_shuffled = calculo_2pcf(galaxies_shuffled, L, spatial_bin_number, n_threads)
+    
+    pcf_shuffled.to_csv(f'Resultados/{path}/Shuffled/PCF/pcf_shuffled{q+part*20}.csv', index=False) # We save the shuffled 2pcf
+    
+    
+    pcf_shuffled_xi = pcf_shuffled['xi']
+    lista_xis.append(pcf_shuffled_xi)
+
+# We unite all the shuffled 2PCF and compute the mean and std
+pcf_shuffled_xi = pd.concat(lista_xis, axis=1)
+
+pcf_shuffled_xi = pcf_shuffled_xi.assign(mean=pcf_shuffled_xi.mean(axis=1))
+pcf_shuffled_xi = pcf_shuffled_xi.assign(std=pcf_shuffled_xi.std(axis=1))
+pcf_shuffled_xi = pcf_shuffled_xi.loc[:, ['mean', 'std']] # We discard all the 2PCFs and maintain the mean and std. Then we save it
+pcf_shuffled_xi.to_csv(f'Resultados/{path}/pcf_shuffled_mean.csv', index=False)
+
 time_end = datetime.now()
 print(f"Initial time...: {time_ini}")
 print(f"Final time.....: {time_end}")
